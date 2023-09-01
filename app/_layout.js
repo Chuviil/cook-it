97348fdd31cc3ba8bff18cb4c45170d236ca0a04
Sebastian Quito <josueq12@gmail.com>
2023-09-01 13:28:25 -0500

    Error Handling _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,16 +1,29 @@
-import {Stack} from "expo-router";
+import {Slot, Stack} from "expo-router";
 import {useFonts} from "expo-font";
 import {Provider} from "../hooks/auth";
 import * as SplashScreen from "expo-splash-screen";
+import {useEffect} from "react";
+
+export {ErrorBoundary} from 'expo-router';
 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    const [fontsLoaded] = useFonts({
+    const [fontsLoaded, error] = useFonts({
         AnekGujaratiBold: require('../assets/fonts/AnekGujarati-Bold.ttf'),
         AnekGujaratiMedium: require('../assets/fonts/AnekGujarati-Medium.ttf'),
         AnekGujaratiRegular: require('../assets/fonts/AnekGujarati-Regular.ttf'),
-    })
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
 
     if (!fontsLoaded) {
         return null;
